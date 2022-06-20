369611a3a0b41b5dacd5f2091732a31c9f486cf6
ogoiddev <diogompantaleao@gmail.com>
2022-06-20 05:17:18 -0300

    update

--- development/my_portfolio/src/routes/index.tsx
+++ development/my_portfolio/src/routes/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
-import Hall from '../pages/Hall/index';
+// import Hall from '../pages/Hall/index';
 import Home from '../pages/Home/index';
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Hall/>}/>
+      <Route path="/" element={<Home/>}/>
       <Route path="/home" element={<Home/>}/>
     </Routes>
   )
